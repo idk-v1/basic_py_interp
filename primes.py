50af7499a156f7d4a1d816ba43ea614bf120d737
idk-v1 <104273594+idk-v1@users.noreply.github.com>
2026-04-19 16:58:19 -0500

    speed up primes test

--- primes.py
+++ primes.py
@@ -2,21 +2,22 @@ print "primes from "
 input i
 print "to "
 input max
+if i <= 2 and 2 <= max:
+	print 2
+	print " "
+if i < 3:
+	i = 3
 while i <= max:
-    if i == 2:
-        print i
-        print " "
-    elif i > 2:
-        ii = 2
-        found = False
-        sq = sqrt i
-        while ii <= sq and not found:
-            if i % ii == 0:
-                found = True
-            else:
-                ii += 1
-        if not found:
-            print i
-            print " "
-    i += 1
+	ii = 2
+	found = False
+	sq = sqrt i
+	while ii <= sq and not found:
+		if i % ii == 0:
+			found = True
+		else:
+			ii += 1
+	if not found:
+		print i
+		print " "
+	i += 2
 println ""
